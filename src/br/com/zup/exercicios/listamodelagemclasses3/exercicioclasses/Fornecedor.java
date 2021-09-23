65480e5d8668dc42485802a47720dcd02cd7a500
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-23 16:40:24 -0300

    Criação da classe Fornecedor.

---
+++ src/br/com/zup/exercicios/listamodelagemclasses3/exercicioclasses/Fornecedor.java
@@ -0,0 +1,43 @@
+package br.com.zup.exercicios.listamodelagemclasses3.exercicioclasses;
+
+public class Fornecedor extends Pessoa {
+    private double valorCredito;
+    private double valorDivida;
+
+    public Fornecedor() {
+
+    }
+
+    public Fornecedor(double valorCredito, double valorDivida) {
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    public Fornecedor(String nome, String endereco, String telefone, double valorCredito, double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    public double getValorCredito() {
+        return valorCredito;
+    }
+
+    public void setValorCredito(double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+
+    public double getValorDivida() {
+        return valorDivida;
+    }
+
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+    public double obterSaldo(){
+        double saldo = valorCredito - valorDivida;
+        return saldo;
+    }
+
+}
