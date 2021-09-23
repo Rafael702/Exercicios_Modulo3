11250a0087b3e3d2f89969b6f75c2a4f5c1ace5b
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-23 17:04:24 -0300

    Aplicando a classe Colaborador na main.

--- src/br/com/zup/exercicios/listamodelagemclasses3/exercicioclasses/Main.java
+++ src/br/com/zup/exercicios/listamodelagemclasses3/exercicioclasses/Main.java
@@ -3,7 +3,10 @@ package br.com.zup.exercicios.listamodelagemclasses3.exercicioclasses;
 public class Main {
     public static void main(String[] args) {
         Fornecedor fornecedor = new Fornecedor("FarmaCon","Rua Fonte Seca", "(11)99560-9090", 1000, 500);
+        Colaborador colab = new Colaborador("Afonso","Rua Fonte Seca","(11)9907-5670",1,1045,10);
 
         System.out.println("R$" + fornecedor.obterSaldo());
+        System.out.println("R$" + colab.calcularSalario());
+
     }
 }
