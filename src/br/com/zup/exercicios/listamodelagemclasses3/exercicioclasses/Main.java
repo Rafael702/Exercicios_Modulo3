c7b690d27ad518b7e1bdc3f20527cffcaf20c7cf
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-23 17:22:35 -0300

    Exercicio Finalizado. Imprimindo o nome e saldo da classe Fornecedor. Imprimindo nome e salário das Classes Colaborador e Administrador.

--- src/br/com/zup/exercicios/listamodelagemclasses3/exercicioclasses/Main.java
+++ src/br/com/zup/exercicios/listamodelagemclasses3/exercicioclasses/Main.java
@@ -6,9 +6,9 @@ public class Main {
         Colaborador colab = new Colaborador("Afonso","Rua Fonte Seca","(11)9907-5670",1,1045,10);
         Administrador adm = new Administrador("Rodrigo","Rua Fonte Seca","(11)99854-4352",2,1500,50,150);
 
-        System.out.println("R$" + fornecedor.obterSaldo());
-        System.out.println("R$" + colab.calcularSalario());
-        System.out.println("R$" + adm.calcularSalario());
+        System.out.println("Saldo do Fornecedor " +  fornecedor.getNome() +": R$" + fornecedor.obterSaldo());
+        System.out.println("Salário do colaborador "+ colab.getNome() + ": R$" + colab.calcularSalario());
+        System.out.println("Salário do administrador " + adm.getNome() + ": R$" + adm.calcularSalario());
 
     }
 }
