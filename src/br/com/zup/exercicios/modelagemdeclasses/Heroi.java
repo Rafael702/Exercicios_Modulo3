3e96d5e519e28fec8f00385e9385998aa8fcee23
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-23 11:49:52 -0300

    Classe Herói. Super Class.

---
+++ src/br/com/zup/exercicios/modelagemdeclasses/Heroi.java
@@ -0,0 +1,43 @@
+package br.com.zup.exercicios.modelagemdeclasses;
+
+public class Heroi {
+    private String nome;
+    private double vida;
+
+    public Heroi() {
+
+    }
+
+    public Heroi(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+
+    public String trocarNome(String nome){
+       return this.nome = nome;
+    }
+
+    public double receberDano(double dano){
+        return dano;
+    }
+
+    public double exibirVida(){
+        return vida;
+    }
+}
