fcecf1b6628a59645e553a1b2d2d2cce22aa87bf
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-24 14:02:49 -0300

    Estruturando a classe com o Polimorfismo estático.

--- src/br/com/zup/exemplos/polimorfismo/exemplo1/Atores.java
+++ src/br/com/zup/exemplos/polimorfismo/exemplo1/Atores.java
@@ -1,5 +1,45 @@
 package br.com.zup.exemplos.polimorfismo.exemplo1;
 
 public class Atores {
+    private String nome;
+    private String papel;
+    private double salario;
 
+    public Atores(String nome) {
+        this.nome = nome;
+    }
+
+    public Atores(double salario) {
+        this.salario = salario;
+    }
+
+    public Atores(String nome, String papel, double salario) {
+        this.nome = nome;
+        this.papel = papel;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPapel() {
+        return papel;
+    }
+
+    public void setPapel(String papel) {
+        this.papel = papel;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
