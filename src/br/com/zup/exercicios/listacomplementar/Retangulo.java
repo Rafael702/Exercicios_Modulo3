175aae84a7ec4beb43f7a238e2d8377d8d352e51
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-24 10:37:23 -0300

    Classe Retangulo. Exercicio 3 - Lista Complementar.

---
+++ src/br/com/zup/exercicios/listacomplementar/Retangulo.java
@@ -0,0 +1,49 @@
+package br.com.zup.exercicios.listacomplementar;
+
+public class Retangulo {
+    private double base, altura;
+
+    public Retangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void mudarValorDosLados(double valorLado, int baseOuAltura) {
+        if (baseOuAltura == 1) {
+            base = valorLado;
+        } else if (baseOuAltura == 2) {
+            altura = valorLado;
+        } else {
+            base = valorLado;
+            altura = valorLado;
+        }
+    }
+
+    public String valorDosLados() {
+        return "Base: " + base + "\nAltura:" + altura;
+    }
+
+    public void calculoAreaEPerimetro() {
+        double area = base * altura;
+        double perimetro = base + altura;
+
+        System.out.println("Área do Retangulo: " + area);
+        System.out.println("Perímetro do Retangulo: " + perimetro);
+    }
+}
