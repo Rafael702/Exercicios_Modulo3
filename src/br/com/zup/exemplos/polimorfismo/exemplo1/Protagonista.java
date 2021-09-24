1c367e73b6a2f538e545e713f92bbb907411d7fe
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-24 14:13:58 -0300

    Estruturando a classe Protagonista.

--- src/br/com/zup/exemplos/polimorfismo/exemplo1/Protagonista.java
+++ src/br/com/zup/exemplos/polimorfismo/exemplo1/Protagonista.java
@@ -1,4 +1,20 @@
 package br.com.zup.exemplos.polimorfismo.exemplo1;
 
-public class Protagonista {
+public class Protagonista extends Ator{
+    private double plr;
+
+    public Protagonista(String nome, String papel, double salario, double imposto, double plr) {
+        super(nome, papel, salario, imposto);
+        this.plr = plr;
+    }
+
+    public double getPlr() {
+        return plr;
+    }
+
+    public void setPlr(double plr) {
+        this.plr = plr;
+    }
+
+
 }
