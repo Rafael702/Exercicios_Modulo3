39dcf7dbc44f7f3cae12fee75a3aa27581e022b1
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-27 10:41:38 -0300

    Classe Morador estruturada.

---
+++ src/br/com/zup/exercicios/exerciciolevelup/Morador.java
@@ -0,0 +1,27 @@
+package br.com.zup.exercicios.exerciciolevelup;
+
+public class Morador {
+        private String nome;
+        private String cpf;
+
+    public Morador(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
