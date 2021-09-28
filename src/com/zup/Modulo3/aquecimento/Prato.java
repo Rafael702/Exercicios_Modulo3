f8a9d3a228091fb365df5e94a0355b2ef6cfab17
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-28 17:08:23 -0300

    Classe Prato, estruturada.

--- src/com/zup/Modulo3/aquecimento/Prato.java
+++ src/com/zup/Modulo3/aquecimento/Prato.java
@@ -1,10 +1,22 @@
 package com.zup.Modulo3.aquecimento;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prato {
+    private Ingredientes ingrediente;
     private String nome;
-    private  double valorDoPrato;
+    private double valorDoPrato;
+
+    List<Ingredientes> listaDeIngredientes = new ArrayList<>();
 
     public Prato(String nome, double valorDoPrato) {
+        this.nome = nome;
+        this.valorDoPrato = valorDoPrato;
+    }
+
+    public Prato(Ingredientes ingrediente, String nome, double valorDoPrato) {
+        this.ingrediente = ingrediente;
         this.nome = nome;
         this.valorDoPrato = valorDoPrato;
     }
@@ -24,4 +36,30 @@ public class Prato {
     public void setValorDoPrato(double valorDoPrato) {
         this.valorDoPrato = valorDoPrato;
     }
+
+    public void adicionarIngredientes(){
+       listaDeIngredientes.add(ingrediente);
+    }
+
+    public Ingredientes getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(Ingredientes ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public List<Ingredientes> getListaDeIngredientes() {
+        return listaDeIngredientes;
+    }
+
+    @Override
+    public String toString(){
+        StringBuffer retorno = new StringBuffer();
+        retorno.append("Nome do prato: " + nome);
+        retorno.append("Valor do Prato: " + valorDoPrato);
+        retorno.append("Ingredientes" + listaDeIngredientes);
+        return retorno.toString();
+    }
+
 }
