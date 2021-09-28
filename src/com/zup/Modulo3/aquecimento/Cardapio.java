aa41e6396f56288be0666daa616acd8fd1d1c8ee
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-28 17:08:55 -0300

    Classe Cardápi, estruturada.

---
+++ src/com/zup/Modulo3/aquecimento/Cardapio.java
@@ -0,0 +1,25 @@
+package com.zup.Modulo3.aquecimento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cardapio {
+    Prato prato;
+
+    List<Prato> listaDePratos = new ArrayList<>();
+
+    public Cardapio(Prato prato) {
+        this.prato = prato;
+    }
+
+    public void adicionarPrato(){
+        listaDePratos.add(prato);
+    }
+
+    public void exibirListaPratos(){
+        for(Prato pratos: listaDePratos){
+        System.out.println("Nome: " + pratos.getNome() + "| Valor: " + pratos.getValorDoPrato()
+        + "| Ingredientes: " + pratos.getListaDeIngredientes());
+        }
+    }
+}
