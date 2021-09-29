f34ef2d20ef7d64921c03a6f21d63d43b77659bc
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-29 11:37:52 -0300

    Classe SistemaRestaurante finalizada,.

---
+++ src/com/zup/Modulo3/aquecimento/SistemaRestaurante.java
@@ -0,0 +1,74 @@
+package com.zup.Modulo3.aquecimento;
+
+import java.util.Scanner;
+
+public class SistemaRestaurante {
+
+    public static Scanner capturarDados(String mensagem) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(mensagem);
+        return sc;
+    }
+
+    public static void menu() {
+        System.out.println("Seja Bem Vindo ao BM Lugar");
+        System.out.println("Digite 1: Para Adicionar Pratos");
+        System.out.println("Digite 2: Para Exibir o Cardápio");
+        System.out.println("Digite 3: Para Sair Do Programa");
+    }
+
+    public static Prato cadastrarPratos() {
+        String nome = capturarDados("Informe o nome do Prato:").nextLine();
+        double valorDoPrato = capturarDados("Informe o valor do Prato:").nextDouble();
+
+        Prato prato = new Prato(nome, valorDoPrato);
+
+        return prato;
+    }
+
+    public static Ingredientes cadastrarIngredientes() {
+        String nome = capturarDados("Nome do Ingrediente:").nextLine();
+
+        Ingredientes ingredientes = new Ingredientes(nome);
+
+        return ingredientes;
+
+    }
+
+    private final static int OPCAO_UM = 1;
+    private final static int OPCAO_DOIS = 2;
+    private final static int OPCAO_TRES = 3;
+
+    public static void executar() {
+
+        Cardapio cardapio = new Cardapio();
+        boolean chave = true;
+
+        while (chave) {
+
+            menu();
+            int option = capturarDados("Digite a opção desejada: ").nextInt();
+
+            if (option == OPCAO_UM) {
+
+                Prato prato = cadastrarPratos();
+                Ingredientes ingredientes = cadastrarIngredientes();
+
+                prato.adicionarIngredientes(ingredientes);
+                cardapio.adicionarPrato(prato);
+
+            } else if (option == OPCAO_DOIS) {
+                cardapio.exibirListaPratos();
+            }else if(option == OPCAO_TRES){
+                System.out.println("Agradecemos por usar o programa");
+                chave = false;
+            }else{
+                System.out.println("Digite uma opção válida, de acordo com o menu.");
+            }
+        }
+        System.out.println("------------------------------------------------------");
+        System.out.println("FIM DO PROGRAMA");
+        System.out.println("------------------------------------------------------");
+    }
+
+}
