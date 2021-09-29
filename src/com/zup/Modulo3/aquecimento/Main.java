b56fef57494a7592a79ca7630227cd7f2200bd81
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-29 11:39:17 -0300

    Execução da classe principal. Usando a classe SistemaRestaurante.

--- src/com/zup/Modulo3/aquecimento/Main.java
+++ src/com/zup/Modulo3/aquecimento/Main.java
@@ -2,12 +2,6 @@ package com.zup.Modulo3.aquecimento;
 
 public class Main {
     public static void main(String[] args) {
-        Ingredientes ingredientes =  new Ingredientes("Massa, Carne moida e molho de tomate");
-        Prato prato = new Prato(ingredientes,"Panqueca", 20);
-        Cardapio cardapio = new Cardapio(prato);
-
-        prato.adicionarIngredientes();
-        cardapio.adicionarPrato();
-        cardapio.exibirListaPratos();
+       SistemaRestaurante.executar();
     }
 }
