ab1223709b1906ff8195e08984ba09b43207dcb6
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-10-05 12:55:56 -0300

    Funcionar método Morador na Main

--- src/br/com/zup/exercicios/abstracao/imobiliaria/Main.java
+++ src/br/com/zup/exercicios/abstracao/imobiliaria/Main.java
@@ -6,13 +6,8 @@ public class Main {
         Imovel imovel = new Imovel("Rua Afonso Padilha","Jorge",450);
         Imovel imovel2 = new Imovel("Rua Condonoente","Francis",750);
         Imovel imovel3 = new Imovel("Rua Patifilio","Robert",450);
-        Morador morador = new Morador("Leopoldo","031.035.687.09");
-        Morador morador2 = new Morador("Leonard","021.067.609.43");
-        Morador morador3 = new Morador("Leopoldo","526.109.628.18");
 
-        imovel.adicionarMoradores(morador);
-        imovel2.adicionarMoradores(morador2);
-        imovel3.adicionarMoradores(morador3);
+        imovel.adicionarMoradores(Sistema.criarMorador());
         catalogo.cadastrarImovel(imovel);
         catalogo.cadastrarImovel(imovel2);
         catalogo.cadastrarImovel(imovel3);
