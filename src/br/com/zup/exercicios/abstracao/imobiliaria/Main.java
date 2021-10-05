68f49438fed9157c0546265ffdd907d0d2ae703b
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-10-05 13:12:31 -0300

    Aplicar métodos de Sistema.

--- src/br/com/zup/exercicios/abstracao/imobiliaria/Main.java
+++ src/br/com/zup/exercicios/abstracao/imobiliaria/Main.java
@@ -3,16 +3,10 @@ package br.com.zup.exercicios.abstracao.imobiliaria;
 public class Main {
     public static void main(String[] args) {
         CatalogoImobiliaria catalogo = new CatalogoImobiliaria();
-        Imovel imovel = new Imovel("Rua Afonso Padilha","Jorge",450);
-        Imovel imovel2 = new Imovel("Rua Condonoente","Francis",750);
-        Imovel imovel3 = new Imovel("Rua Patifilio","Robert",450);
+        Imovel imovel = new Imovel();
 
-        imovel.adicionarMoradores(Sistema.criarMorador());
-        catalogo.cadastrarImovel(imovel);
-        catalogo.cadastrarImovel(imovel2);
-        catalogo.cadastrarImovel(imovel3);
-        System.out.println(imovel.toString());
-
+        catalogo.cadastrarImovel(Sistema.criarImovel());
+        System.out.println(catalogo);
 
     }
 }
