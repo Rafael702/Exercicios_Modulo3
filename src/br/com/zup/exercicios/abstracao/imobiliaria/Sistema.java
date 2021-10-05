52b7df54728c47f550018ae142354ec6bd1fef85
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-10-05 13:13:11 -0300

    Criar imóvel.

--- src/br/com/zup/exercicios/abstracao/imobiliaria/Sistema.java
+++ src/br/com/zup/exercicios/abstracao/imobiliaria/Sistema.java
@@ -4,18 +4,30 @@ import java.util.Scanner;
 
 public class Sistema {
 
-    public static Scanner capturarDados(String mensagem){
+    public static Scanner capturarDados(String mensagem) {
         Scanner sc = new Scanner(System.in);
         System.out.println(mensagem);
         return sc;
     }
 
-    public static Morador criarMorador(){
+
+    public static Morador criarMorador() {
         String nome = capturarDados("Informe o nome do Morador: ").nextLine();
         String cpf = capturarDados("Informe o CPF do " + nome + ":").nextLine();
 
-        Morador morador = new Morador(nome,cpf);
+        Morador morador = new Morador(nome, cpf);
         return morador;
     }
+
+    public static Imovel criarImovel() {
+        String rua = capturarDados("Informe o nome da Rua em que está localizado o imóvel:").nextLine();
+        String funcionario = capturarDados("Informe o nome do Funcionário:").nextLine();
+        double valorAluguel = capturarDados("Informe o valor do Aluguel").nextDouble();
+
+        Imovel imovel = new Imovel(rua, funcionario, valorAluguel,criarMorador());
+
+        return imovel;
+    }
+
 
 }
